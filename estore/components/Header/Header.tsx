3dbfffcd302b1aa58f7d434e74eb1a7d0de26ce2
sunilfbd <sunil.ahuja@akqa.com>
2022-03-03 17:20:16 +0530

    created basic Header

--- estore/components/Header/Header.tsx
+++ estore/components/Header/Header.tsx
@@ -1,6 +1,36 @@
 import React from "react";
-import { PageHeader } from "./pageHeader";
+import Link from "next/link";
+import Box from "@mui/material/Box";
+import {
+  PageHeader,
+  ServiceMenu,
+  ServiceMenuItem,
+  LogoWrapper,
+} from "./pageHeader";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
-const Header = () => <PageHeader>This is Header..</PageHeader>;
+const Header = () => {
+  return (
+    <PageHeader>
+      <LogoWrapper>
+        <Box>Logo goes here</Box>
+      </LogoWrapper>
+
+      <ServiceMenu>
+        <ServiceMenuItem>
+          <Link href="/cart">
+            <ShoppingCartIcon fontSize="large" />
+          </Link>
+        </ServiceMenuItem>
+        <ServiceMenuItem>
+          <Link href="/cart">
+            <BookmarkIcon fontSize="large" />
+          </Link>
+        </ServiceMenuItem>
+      </ServiceMenu>
+    </PageHeader>
+  );
+};
 
 export default Header;
--- estore/components/Header/pageHeader.tsx
+++ estore/components/Header/pageHeader.tsx
@@ -1,12 +1,33 @@
-// import styled, { css } from "styled-components";
 import styled from "styled-components";
 
 export const PageHeader = styled.div`
   min-height: 80px;
   display: block;
-  // background-color: ${(props) => props.theme.bgColor.secondary};
   color: ${(props) => props.theme.color.primary};
   padding: 10px;
   border-bottom: 1px solid ${(props) => props.theme.color.primary};
   margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+export const LogoWrapper = styled.div`
+  width: 200px;
+`;
+
+export const ServiceMenu = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  margin-left: auto;
+`;
+
+export const ServiceMenuItem = styled.li`
+  list-style-type: none;
+  padding: 0;
+  margin: 0 0 0 20px;
+  &:first-child {
+    margin-left: 0;
+  }
 `;
